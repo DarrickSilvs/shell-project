97787c5cf5baf948622b2162b7d1058de784ea23
Darrick <z5510366@ad.unsw.edu.au>
2025-02-21 03:31:46 +1100

    echo

--- app/main.py
+++ app/main.py
@@ -7,15 +7,14 @@ def main():
         sys.stdout.write("$ ")
 
         # Wait for user input
-        command = input()
-        if str(command) == "exit 0":
-            break
-
-        ls = command.split(" ")
-        if str(ls[0]) == "echo":
-            print(' '.join(ls[1:]))
-        else:
-            print(f"{command}: command not found")
+        command, *args = input().split(" ")
+        match command:
+            case "exit":
+                break
+            case "echo":
+                print(" ".join(args))
+            case default:
+                sys.stdout.write(f"{command}: command not found")
 
 
 if __name__ == "__main__":
