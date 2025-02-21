e96102ad81f49464d3ed1c1cad2818adba579c27
Darrick <z5510366@ad.unsw.edu.au>
2025-02-21 13:49:48 +1100

    run executable

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import subprocess
 
 def main():
     # Uncomment this block to pass the first stage
@@ -19,21 +19,28 @@ def main():
                 if args[0] in builtin:
                     print(f"{args[0]} is a shell builtin")
                 else:
-                    pathList =os.environ["PATH"].split(':')
-                    flag = False
-                    for i in pathList:
-                        if os.path.exists(f"{i}/{args[0]}") == True:
-                            flag = True
-                            print(f"{args[0]} is {i}/{args[0]}")
-                            break
-                    
-                    if flag == False:
+                    path = checkPathFile(args[0])
+                    if path != False:
+                        print(f"{args[0]} is {path}/{args[0]}")
+                    else:
                         print(f"{args[0]}: not found")
             case _:
-                print(f"{command}: command not found")
+                path = checkPathFile(sys.argv[0])
+                if path != False:
+                    subprocess.run(sys.argv[0] + sys.argv[1])
+                else:
+                    print(f"{command}: command not found")
     
     return
 
+def checkPathFile(file):
+    pathList = os.environ["PATH"].split(':')
+    flag = False
+    for i in pathList:
+        if os.path.exists(f"{i}/{file}") == True:
+            flag = i
+            break
+    return flag
 
 if __name__ == "__main__":
     main()
